a1181dd06eb941452605c1fef2c7af4f202fb3fc
sid-who <gsidhu@csumb.edu>
2019-09-27 17:41:31 -0700

    Sprint2 Files added. Checkout, Cart, and Order confirmation. Expanded Product page to include a details route for specific items.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'; //homepage
 import { SignInComponent } from './sign-in/sign-in.component'; //sign in page
 import { ProductsComponent } from './products/products.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { DetailsComponent } from './details/details.component';
+import { OrderedComponent } from './ordered/ordered.component';
+import { CartComponent } from './cart/cart.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-in', component: SignInComponent },
-  { path: 'products', component: ProductsComponent }
+  { path: 'products', component: ProductsComponent },
+  { path: 'checkout', component: CheckoutComponent},
+  { path: 'details', component: DetailsComponent},
+  { path: 'ordered', component: OrderedComponent},
+  { path: 'cart', component: CartComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { ProductsComponent } from './products/products.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { DetailsComponent } from './details/details.component';
+import { OrderedComponent } from './ordered/ordered.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SignInComponent,
-    ProductsComponent
+    ProductsComponent,
+    CheckoutComponent,
+    DetailsComponent,
+    OrderedComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
